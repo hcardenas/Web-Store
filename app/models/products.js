941e558249f179cae86e39ae8a756713639aa9ab
Helmut Cardenas <aleksey89col@gmail.com>
2017-10-03 23:19:28 -0400

    init

---
+++ app/models/products.js
@@ -0,0 +1,8 @@
+module.exports = function(sequelize, DataTypes) {
+  var Product = sequelize.define("Product", {
+    // Giving the Product model a name of type STRING
+    name: DataTypes.STRING
+  });
+
+  return Product;
+};
---
+++ app/routes/api-routes.js
@@ -0,0 +1,39 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our Todo model
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all 
+  app.get("/api/posts/", function(req, res) {
+    db.Product.findAll({}).then(function(dbAuthor) {
+      res.json(dbAuthor);
+    });
+  });
+
+
+
+  // POST route for saving a new product
+  app.post("/api/posts", function(req, res) {
+    console.log(req.body);
+    
+  });
+
+  // DELETE route for deleting 
+  app.delete("/api/posts/:id", function(req, res) {
+    
+  });
+
+  // PUT route for updating posts
+  app.put("/api/posts", function(req, res) {
+    
+  });
+};
