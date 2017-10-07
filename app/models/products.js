bed7a8a0bc30144c6be0e6cedaec0aa41b6a6a95
helmut cardenas <aleksey89col@gmail.com>
2017-10-07 12:52:35 -0400

    changed database

--- app/models/products.js
+++ app/models/products.js
@@ -2,6 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var Product = sequelize.define("Product", {
     // Giving the Product model a name of type STRING
     name: DataTypes.STRING
+    categories: DataTypes.STRING
   });
 
   return Product;
